34018b8ee3de7641de1fbe4c36f093e6400455fe
Alex Nichol <unixpickle@gmail.com>
2017-04-03 12:40:31 -0400

    basic fetching works

---
+++ README.md
@@ -0,0 +1,8 @@
+# audioset
+
+This is a tool for fetching data from [AudioSet](https://research.google.com/audioset/download.html), a dataset of audio events.
+
+# Dependencies
+
+ * ffmpeg
+ * youtube-dl
---
+++ download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# fetch_clip(videoID, startTime, endTime)
+fetch_clip() {
+  echo "Fetching $1 ($2 to $3)..."
+  outname="$1_$2"
+  if [ -f "${outname}.wav.gz" ]; then
+    echo "Already have it."
+    return
+  fi
+
+  youtube-dl https://youtube.com/watch?v=$1 \
+    --quiet --extract-audio --audio-format wav \
+    --output "$outname.%(ext)s"
+  if [ $? -eq 0 ]; then
+    # If we don't pipe `yes`, ffmpeg seems to steal a
+    # character from stdin. I have no idea why.
+    yes | ffmpeg -loglevel quiet -i "./$outname.wav" \
+      -ss "$2" -c copy -to "$3" "./${outname}_out.wav"
+    mv "./${outname}_out.wav" "./$outname.wav"
+    gzip "./$outname.wav"
+  else
+    # Give the user a chance to Ctrl+C.
+    sleep 1
+  fi
+}
+
+grep -E '^[^#]' | while read line
+do
+  fetch_clip $(echo "$line" | sed -E 's/, / /g')
+done
