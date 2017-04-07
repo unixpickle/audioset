3ad39d517ddbfea41af59eda74f246ccb7806670
Alex Nichol <unixpickle@gmail.com>
2017-04-07 10:51:21 -0400

    tool for finding corrupt samples

---
+++ find_bad/main.go
@@ -0,0 +1,39 @@
+// Command find_bad finds corrupted audio samples in a
+// directory of AudioSet samples.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"github.com/unixpickle/audioset/metaset"
+	"github.com/unixpickle/essentials"
+)
+
+func main() {
+	var dirPath string
+	flag.StringVar(&dirPath, "dir", "", "path to sample directory")
+	flag.Parse()
+
+	if dirPath == "" {
+		essentials.Die("Required flag: -dir. See -help.")
+	}
+
+	listing, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		essentials.Die(err)
+	}
+
+	for _, item := range listing {
+		if strings.HasSuffix(item.Name(), ".wav.gz") {
+			path := filepath.Join(dirPath, item.Name())
+			sample := metaset.Sample{Path: path}
+			if _, err := sample.Read(); err != nil {
+				fmt.Println(path)
+			}
+		}
+	}
+}
